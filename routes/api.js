676dd263fbe10145955310941c1466d014aa6426
Steve Golden <sgolden2525@outlook.com>
2019-05-20 02:08:41 -0500

    Added create/post for new books
    Added the html and JS for posting the new book, title, and comment to the database.
    
    Issue:  It is adding the book twice.

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,35 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
+const expect = require('chai').expect;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const mongoose = require('mongoose');
+const Schema = require('mongoose').Schema;
+const shortid = require('shortid');
+const multer = require('multer');
+const upload = multer();
 const MONGODB_CONNECTION_STRING = process.env.DB;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
+
+mongoose.connect('mongodb://localhost:27017/BookLibrary', { useNewUrlParser: true }, (err) => {
+  if (!err) {
+    console.log("Database connection successful")
+  } else {
+    console.log("Database is not connected: " + err)
+  }
+})
+
+var bookSchema = new Schema({
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  title: String,
+  comments: [String]
+})
+
+var book = mongoose.model('books', bookSchema);
 
 module.exports = function (app) {
 
@@ -22,9 +46,27 @@ module.exports = function (app) {
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
 
-    .post(function (req, res) {
+    .post(upload.array(), function (req, res) {
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
+      console.log("BookTitleToAdd: " + req.body.createTitle)
+      console.log("BookCommentToAdd: " + req.body.createComment)
+
+      var newBook = new book({
+        _id: shortid.generate(),
+        title: req.body.createTitle,
+        comments: req.body.createComment    
+      })
+
+       newBook.save(newBook, function (err, issue) {
+        console.log("BOOK: " + book._id)        
+        if (err) {
+          res.send("Book could not be created")
+        }
+        else {
+          res.send("Book successfully created. <br> Book id number is: " + issue._id)
+        }
+      })
     })
 
     .delete(function (req, res) {
